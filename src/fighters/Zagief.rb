cd957d70e3dd3f904b1567650fd1a282ac733d09
Alastair McGilloway <alastairmcgilloway@hotmail.com>
2021-04-28 10:50:09 +0100

    Updates zagiefs mmoves

--- src/fighters/Zagief.rb
+++ src/fighters/Zagief.rb
@@ -10,6 +10,7 @@ class Zagief < Fighter
     @taunt = "Bow before me pitiful mortals!"
 
     move1 = Move.new({ name: "Punch", damage: 5, hit_chance: 80 })
+    move2 = Move.new({name: "Haymaker", damage: 10, hit_chance:60})
 
     @moves = [move1, move2, move3]
   end
